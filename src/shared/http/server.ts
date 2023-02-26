133efd7efa79208a723c6b9e32bb1be8e7c2880f
Josafa Luna <josafaluna@gmail.com>
2023-02-26 17:43:32 -0300

    feature (errors), Customizing Application Error Handling

---
+++ src/shared/http/server.ts
@@ -0,0 +1,33 @@
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes/index';
+import AppError from '@shared/erros/AppError';
+
+const app = express();
+app.use(cors);
+app.use(express.json());
+
+// Routes
+app.use(routes);
+
+// Middleware para tratamento de erro
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    // valida se o error é uma instancia da Classe AppError
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
+
+app.listen(3333, () => {
+  console.log('Running server pot 3333!');
+});
