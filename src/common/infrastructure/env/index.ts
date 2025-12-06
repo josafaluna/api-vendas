a95fdf6337fbd7e7e115abe506b7d48e25b68327
Josafá Luna <josafaluna@gmail.com>
2025-12-06 18:40:41 -0300

    feat: configura docker-compose

--- src/common/infrastructure/env/index.ts
+++ src/common/infrastructure/env/index.ts
@@ -1,6 +1,6 @@
-import 'dotenv/config'
-import { z } from 'zod'
-import { AppError } from '../../domain/errors/app-error'
+import 'dotenv/config';
+import { z } from 'zod';
+import { AppError } from '../../domain/errors/app-error';
 
 const envSchema = z.object({
   NODE_ENV: z
@@ -8,13 +8,13 @@ const envSchema = z.object({
     .default('development'),
   PORT: z.coerce.number().default(3333),
   API_URL: z.string().default('http://localhost:3333'),
-})
+});
 
-const _env = envSchema.safeParse(process.env)
+const _env = envSchema.safeParse(process.env);
 
 // valida se houve ou não error na validação
 if (!_env.success) {
-  throw new AppError('Inválid environment variable is missing')
+  throw new AppError('Inválid environment variable is missing');
 }
 
-export const env = _env.data
+export const env = _env.data;
